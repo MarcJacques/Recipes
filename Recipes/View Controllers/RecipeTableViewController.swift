824f11c95572934751503cd3561c505289c87d90
Marc <marc.daniel.jacques@gmail.com>
2020-03-11 23:23:46 -0400

    MVP

--- Recipes/View Controllers/RecipeTableViewController.swift
+++ Recipes/View Controllers/RecipeTableViewController.swift
@@ -11,95 +11,107 @@ import UIKit
 class RecipeTableViewController: UITableViewController {
     
     // MARK: - Properties
+    let networkClient = RecipesNetworkClient()
+    var recipes: [Recipe] = [] {
+        didSet {
+            tableView.reloadData()
+        }
+    }
     
     let searchController = UISearchController(searchResultsController: nil)
     var filteredRecipes: [Recipe] = []
-    var isSearchEmpty: Bool {
-        return searchController.searchBar.text?.isEmpty ?? true 
-    }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.reloadData()
+        
         searchController.searchResultsUpdater = self
         searchController.obscuresBackgroundDuringPresentation = false
         searchController.searchBar.placeholder = "Search Recipes"
         navigationItem.searchController = searchController
         definesPresentationContext = true
+        
+        networkClient.fetchRecipes { allRecipes, error in
+                if let error = error {
+                    NSLog("Error while fetching all recipes: \(error)")
+                    return
+                }
+                DispatchQueue.main.async {
+                    if let allRecipes = allRecipes {
+                        self.recipes = allRecipes
+                    }
+                }
+            }
+        }
+ 
 
-    
+    var isSearchEmpty: Bool {
+        return searchController.searchBar.text?.isEmpty ?? true
+    }
+    var isFiltering: Bool {
+        return searchController.isActive && !isSearchEmpty
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
+       if isFiltering{
+                return filteredRecipes.count
+            }
+            return recipes.count
+        }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RecipeCell", for: indexPath)
+        let recipe: Recipe
+        if isFiltering {
+            recipe = filteredRecipes[indexPath.row]
+        } else {
+            recipe = recipes[indexPath.row]
+        }
+        cell.textLabel?.text = recipe.name
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+        // MARK: - Functions and methods
+    
+    func filterContentForSearchText(_ searchText: String) {
+        filteredRecipes = recipes.filter { (recipe: Recipe) -> Bool in
+            return recipe.name.lowercased().contains(searchText.lowercased())
+        }
+        tableView.reloadData()
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
+    
+        // MARK: - Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+            guard
+                segue.identifier == "DetailVCSegue",
+                let indexPath = tableView.indexPathForSelectedRow,
+                let detailVC = segue.destination as? RecipeDetailViewController
+                else { return }
+            // Pass the selected object to the new view controller.
+            let recipe: Recipe
+            if isFiltering {
+                recipe = filteredRecipes[indexPath.row]
+            } else {
+                recipe = recipes[indexPath.row]
+            }
+            detailVC.recipe = recipe
+        }
     }
-    */
-//    func filterContentForSearchText( _searchText: String, category: Recipe.Category)
-}
+
+
 
 extension RecipeTableViewController: UISearchResultsUpdating {
     func updateSearchResults(for searchController: UISearchController) {
-        //Todo
+         let searchBar = searchController.searchBar
+               filterContentForSearchText(searchBar.text!)
     }
 }
